4a457ddbfaa7c6745bffadf796886d5597b38b74
grudgecat <grudgecat@protonmail.com>
2021-05-24 06:27:33 -0700

    built intitial HTML + CSS framewk, added JS file with initial timer event

---
+++ assets/gamelogic.js
@@ -0,0 +1,25 @@
+var timerEl = document.getElementById('currTime');
+var myButton = document.getElementById('gobutton');
+// console.log(myButton); 
+
+myButton.addEventListener("click", countdown);
+
+function countdown() {
+  //counter starts with 90 seconds on clock
+  var timeLeft = 90;
+  // Use the `setInterval()` method to 1 sec countdown
+  var timeInterval = setInterval(function () {
+    // As long as the `timeLeft` is greater than 1
+    if (timeLeft > 1) {
+      // Set the `textContent` of `timerEl` to show time left
+      timerEl.textContent = timeLeft;
+      // Decrement `timeLeft` by 1
+      timeLeft--;
+    } else {
+      // Time gets to zero, stop timer & countdown
+      timerEl.textContent = "0";
+      clearInterval(timeInterval);
+      //for future use - end the game
+    }
+  }, 1000);
+}
