e6b3a5699f19e7bbf606dd48e35655e134365c23
grudgecat <grudgecat@protonmail.com>
2021-05-26 16:32:37 -0700

    added answer randomizer function

--- assets/gamelogic.js
+++ assets/gamelogic.js
@@ -1,26 +1,28 @@
 //DECLARE GLOBAL VARIABLES
+//triviaQuestionList is set in separate script, contains all questions/answers for game
+// using localStorage.setItem("triviaQuestionList", JSON.stringify(trivaQuestionList));
 var score = 0;
 var timeLeft = 90;
 var playerInitials = "abc";
+var randomizeAnswersArray = []; 
 var triviaQuestion = {
 
 }
 var questionList = [];
 
-// var numOfQuestions = 50;
 
 
 //DECLARE VARS TO 'GET' ELEMENTS ON PAGE
 var timerEl = document.getElementById('currTime');
-var myButton = document.getElementById('gobutton');
-// console.log(myButton); 
+var buttonEl = document.getElementById('gobutton');
 
-myButton.addEventListener("click", countdown);
+
+buttonEl.addEventListener("click", countdown);
 
 function countdown() {
   //counter starts with 90 seconds on clock
   var timeLeft = 90;
-  myButton.style.display = "none";
+buttonEl.style.display = "none";
 
   // Use the `setInterval()` method to 1 sec countdown
   var timeInterval = setInterval(function () {
@@ -39,13 +41,45 @@ function countdown() {
   }, 1000);
 
   displayQuestion();
+  randomizeAnswersArray = makeRandomFourArray();
+  console.log("randomize answers array is " + randomizeAnswersArray);
+}
+
+function makeRandomFourArray() {
+  var randomFour = [];
+  while (randomFour.length !== 4 && randomFour.length < 5) {
+    var randy =  Math.floor(Math.random() * 4);
+    if (!randomFour.includes(randy))
+    {
+    randomFour.push(randy);
+    }
+  }
+  return randomFour;
 }
 
 function displayQuestion() {
 
   var tqList = JSON.parse(localStorage.getItem("triviaQuestionList"));
+  var tempList = [];
   
   var j = 1;
   document.getElementById("triviaQuestion").innerHTML = tqList[j].question; 
+
+  console.log("tempList length is " + document.getElementById("answerList").children.length);
+  tempList = document.getElementById("answerList").children;
+
+  
+  tempList[2].innerHTML = tqList[j].filler1; 
+
+
+  // document.getElementById("answerList.child").innerHTML = tqList[j].filler1; 
+
+  // console.log(tempList);
+  // console.log(tempList[j]);
+  
+
+  
 }
+
+
 
